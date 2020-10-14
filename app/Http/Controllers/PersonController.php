729c2874a3d2f8470c5d057fb1aeaa8c5d2b2594
Andri Gunawan <andrign20@gmail.com>
2020-10-14 21:42:35 +0700

    add resource collection

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Http\Resources\PersonResource;
+use App\Http\Resources\PersonResourceCollection;
 use Illuminate\Http\Request;
 use App\Person;
 
@@ -10,10 +11,23 @@ class PersonController extends Controller
     /**
      *  @params Person $person
      *  @return PersonResource
+     * 
      */
 
     public function show(Person $person): PersonResource
     {
         return new PersonResource($person);
+    }
+
+    /**
+     * 
+     *  @return PersonResourceCollection
+     * 
+     */
+
+    public function index(): PersonResourceCollection
+    {
+        return new PersonResourceCollection(Person::paginate());
+
     }
 }
